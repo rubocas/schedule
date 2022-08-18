dcb2a5a2236bff2d5748f9b4ba4109e5efeba617
Rubens Kappaun <rubens.kappaun@autotrac.com.br>
2022-08-18 19:54:56 -0300

    troca de url

--- cron/AgendaProfissional.js
+++ cron/AgendaProfissional.js
@@ -5,7 +5,7 @@ var request = require('request');
 
 function AgendaProfissional()
 {
-    request('https://agendamento.institutotozatodepsicologia.com/emailtemplate/Teste', function (error, response, body) {
+    request('https://agendamento.institutotozatodepsicologia.com/tarefa/AtualizarTarefa?tipotarefa=1', function (error, response, body) {
         console.log('error:', error); // Print the error if one occurred
         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
         console.log('body:', body); // Print the HTML for the Google homepage.
