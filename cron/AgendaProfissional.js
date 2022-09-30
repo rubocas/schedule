aff2cffbe2e39a96ce932f49ca5f97599f9af1c6
Rubens Kappaun <rubens.kappaun@autotrac.com.br>
2022-09-30 11:13:53 -0300

    desativado prof

--- cron/AgendaProfissional.js
+++ cron/AgendaProfissional.js
@@ -3,11 +3,12 @@ var request = require('request');
 
 function AgendaProfissional()
 {
+    /*
     request('https://agendamento.institutotozatodepsicologia.com/tarefa/agendaprofissional', function (error, response, body) {
         console.log('error:', error); // Print the error if one occurred
         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
         console.log('body:', body); // Print the HTML for the Google homepage.
-      });
+      });*/
       
     console.log("Agenda profissional!");
 }
