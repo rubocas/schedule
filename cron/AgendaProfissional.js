f8cb7614bbfc770930d97b8b4bc9b7d2fa9b85ed
Rubens Kappaun <rubens.kappaun@autotrac.com.br>
2022-10-06 19:12:56 -0300

    troca de tempo

--- cron/AgendaProfissional.js
+++ cron/AgendaProfissional.js
@@ -3,16 +3,16 @@ var request = require('request');
 
 function AgendaProfissional()
 {
-    /*
+    
     request('https://agendamento.institutotozatodepsicologia.com/tarefa/agendaprofissional', function (error, response, body) {
         console.log('error:', error); // Print the error if one occurred
         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
         console.log('body:', body); // Print the HTML for the Google homepage.
-      });*/
+      });
       
     console.log("Agenda profissional!");
 }
 
-module.exports = cron.schedule('*/59 * * * *', AgendaProfissional, {
+module.exports = cron.schedule('0 * * * *', AgendaProfissional, {
     schedule : false
 });
